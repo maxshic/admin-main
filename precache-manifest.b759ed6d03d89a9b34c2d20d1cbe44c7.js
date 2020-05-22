02a3b9c25c5b2ff759281d42346a348e3cd06743
teng520 <teng520@users.noreply.github.com>
2020-05-22 10:14:29 +0000

    deploy: 6a726fcef746fbf9274c81a06896ef118fe27dfb

---
+++ precache-manifest.b759ed6d03d89a9b34c2d20d1cbe44c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b522b05777da5c30e0deada4af39732",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de9fb8f0581d539c38d7",
+    "url": "./static/css/2.636012f9.chunk.css"
+  },
+  {
+    "revision": "6d417e2e265f684de7f8",
+    "url": "./static/css/main.8526ccc5.chunk.css"
+  },
+  {
+    "revision": "de9fb8f0581d539c38d7",
+    "url": "./static/js/2.f3761812.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f3761812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d417e2e265f684de7f8",
+    "url": "./static/js/main.7b9a12ff.chunk.js"
+  },
+  {
+    "revision": "2169c4c91c7477aeac83",
+    "url": "./static/js/runtime-main.8df7975f.js"
+  }
+]);
