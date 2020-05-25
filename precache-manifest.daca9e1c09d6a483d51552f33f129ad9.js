99dba8813031dd6356f1bdf187f1cc2476e85fe3
teng520 <teng520@users.noreply.github.com>
2020-05-25 10:46:54 +0000

    deploy: acfc2691362499232428e3bf3ddf618ce56c03a9

---
+++ precache-manifest.daca9e1c09d6a483d51552f33f129ad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1253f951baadc2d0f6de476e8842919",
+    "url": "./index.html"
+  },
+  {
+    "revision": "234372dc41c34faaa553",
+    "url": "./static/css/2.fd119cf2.chunk.css"
+  },
+  {
+    "revision": "8c184737935b2ef7dd11",
+    "url": "./static/css/main.283dce0d.chunk.css"
+  },
+  {
+    "revision": "234372dc41c34faaa553",
+    "url": "./static/js/2.f7145ee0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f7145ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c184737935b2ef7dd11",
+    "url": "./static/js/main.7cc6b750.chunk.js"
+  },
+  {
+    "revision": "2169c4c91c7477aeac83",
+    "url": "./static/js/runtime-main.8df7975f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a88be1b35a83f3c219bad8270146cbf.js"
+  "./precache-manifest.daca9e1c09d6a483d51552f33f129ad9.js"
 );
 
 self.addEventListener('message', (event) => {
